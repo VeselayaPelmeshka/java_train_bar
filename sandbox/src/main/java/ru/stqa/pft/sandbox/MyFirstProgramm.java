4857bce6661171a5394787b2589603c32ecc3ed4
VeselayaPelmeshka <veselayapelmeshka@gmail.com>
2017-02-17 02:38:50 +0400

    Усложнили, появились функции

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -3,6 +3,23 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgramm {
 
 	public static void main (String[] args){
-		System.out.println("Hello world");
+		hello("Danil");
+		System.out.println(area(9));
+		System.out.println(area(4,7));
+
 	}
+
+	public static void  hello(String somebody){
+
+		System.out.println("Hello " + somebody + " !");
+	}
+
+	public  static  double area(double n){
+		return n*n;
+	}
+
+	public  static double area(double a, double b){
+		return a*b;
+	}
+
 }
